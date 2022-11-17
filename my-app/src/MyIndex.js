3e57d52d5b93b61f6dadc64b842c81b0b60d3b6d
Misida Krakulli <misidakrakulli@gmail.com>
2022-11-17 20:33:44 +0100

    update

--- my-app/src/MyIndex.js
+++ my-app/src/MyIndex.js
@@ -3,4 +3,4 @@ import React from 'react';
 import {App} from './App';
  const Helloelement = <App/>
  const root = document.querySelector('#root')
- ReactDOM.render(App, root)
+ ReactDOM.render(Helloelement, root)
