e1db120c67aa065a31f59f7413401cc8a0c0cc26
Misida Krakulli <misidakrakulli@gmail.com>
2022-11-18 21:52:53 +0100

    update

--- my-app/src/MyIndex.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import {App} from './App';
- const Helloelement = <App/>
- const root = document.querySelector('#root')
- ReactDOM.render(Helloelement, root)
