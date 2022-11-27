55e2fdeb048192d087a4ec87b5d65d3886793a2b
Misida Krakulli <misidakrakulli@gmail.com>
2022-11-27 22:56:51 +0100

    function components 1

--- my-app/Context 02.txt
+++
@@ -1,1 +0,0 @@
-Consume the `LanguageContext` within the `DisplayLanguage` component by using the context consumer, and show the selected language in an `h1` tag.
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,13 @@
 import React from "react";
-import { DisplayLanguage } from "./DisplayLanguage";
-import { LanguageContext } from "./LanguageContext";
+import { Welcome } from "./Welcome";
 
 
 export class App extends React.Component {
-  state={
-    language: "en",
-  }
-ChangeLanguage=(event)=>{
-  this.setState({
-    language: event.target.value
-  })
-}
+  
 
   render() {
     return (
-      <div>
-       <select value={this.state.language} onChange={this.ChangeLanguage}>
-         <option value="en">English</option>
-         <option value="it">Italiano</option>
-       </select>
-       <LanguageContext.Provider value={this.state.language}>
-         <DisplayLanguage/>
-       </LanguageContext.Provider>
-      </div>
-    );
+      <Welcome name='Lea' age='20'/>
+    )
   }
 }
