1272a10da62bad17e3fb6b3d9935fb6dd21703e6
Misida Krakulli <misidakrakulli@gmail.com>
2022-12-02 12:38:17 +0100

    useContext

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,26 @@
-import {React} from 'react';
-import { CarDetails } from './CarDetails';
+import {React, useState} from 'react';
+import { DisplayLanguage } from './DisplayLanguage';
+import { LanguageContext } from './LanguageContext';
+import { Translation } from './Translation';
 
 
 export function App() {
- 
+ const [ language, setLanguage ] = useState('en')
+
+ function handleChangeLanguage(event){
+    setLanguage(event.target.value)
+ }
 
     return (
      <div>
-     <CarDetails/>
+       <select value={language} onChange={handleChangeLanguage}>
+         <option value="en">English</option>
+         <option value="it">Italiano</option>
+       </select>
+     <LanguageContext.Provider value={language}>
+        <DisplayLanguage/>
+        <Translation/>
+     </LanguageContext.Provider>
      </div>
     )
   }
--- my-app/src/DisplayLanguage.js
+++ my-app/src/DisplayLanguage.js
@@ -1,30 +1,40 @@
-import React from "react";
-import { LanguageContext } from "./LanguageContext";
+//import React from "react";
+//import { LanguageContext } from "./LanguageContext";
 
-const strings = {
-    en: {
-        selectedLanguage: 'What a beautiful day!',
-    },
-    it: {
-        selectedLanguage: 'Che bella giornata!'
-    }
 
-}
 
-export class DisplayLanguage extends React.Component{
-    render(){
-        return(
-            <div>
-                <LanguageContext.Consumer>
-                    {(language)=>{
-                        return <h1>
-                            {strings[language].selectedLanguage}
-                        </h1>
+//const strings = {
+//    en: {
+//        selectedLanguage: 'What a beautiful day!',
+ //   },
+ //   it: {
+ //       selectedLanguage: 'Che bella giornata!'
+//    }
 
-                    }}
-                </LanguageContext.Consumer>
+//}
+
+//export class DisplayLanguage extends React.Component{
+ //   render(){
+ //       return(
+  //          <div>
+ //               <LanguageContext.Consumer>
+ //                   {(language)=>{
+ //                       return <h1>
+ //                           {strings[language].selectedLanguage}
+ //                       </h1>
+
+ //                   }}
+ //               </LanguageContext.Consumer>
                
-            </div>
-        )
-    }
+ //           </div>
+//        )
+ //   }
+//}
+import { useContext } from "react"
+import { LanguageContext } from "./LanguageContext"
+
+export function DisplayLanguage(){
+ const language = useContext(LanguageContext)
+
+    return <h3>Current language : {language}</h3>
 }
