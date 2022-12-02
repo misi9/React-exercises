849409a0076a63a69b406e647aa8a395bfcee4be
Misida Krakulli <misidakrakulli@gmail.com>
2022-12-02 15:36:33 +0100

    path /counter added that renders the Counter component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import {React} from 'react';
 import { Routes,Route } from "react-router-dom"
 import { Welcome } from './Welcome';
 import { Container } from './Container';
+import { Counter } from './Counter';
 
 
 
@@ -12,6 +13,7 @@ export function App() {
       <Container title="Welcome girl!!">
       <Routes>
           <Route path='/' element={<Welcome name="Misida" age={28}/>}/>
+          <Route path='/counter' element={<Counter/>}/>
         </Routes>
         </Container>
     )
