1d9c04f35f64285cb579244f1a07ae618072e8fd
Misida Krakulli <misidakrakulli@gmail.com>
2022-12-02 18:06:00 +0100

    react router 5

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import { Welcome } from './Welcome';
 import { Container } from './Container';
 import { Counter } from './Counter';
 import { ShowGithubUser } from './ShowGithubUser';
+import { NotFound } from './NotFound';
 
 
 
@@ -21,11 +22,12 @@ export function App() {
         <button onClick={navigatePages} value="./welcome">Welcome</button>
         <button onClick={navigatePages} value="./counter">Let's Count</button>
         <button onClick={navigatePages} value="users/misi9">L'accesso è stato effettuato da</button>
-
+        
       <Routes>
           <Route path='/welcome' element={<Welcome name="Misida" age={28}/>}/>
           <Route path='/counter' element={<Counter/>}/>
           <Route path="users/:username" element={<ShowGithubUser/>} />
+          <Route path="*" element={<NotFound/>} />
         </Routes>
         </Container>
     )
